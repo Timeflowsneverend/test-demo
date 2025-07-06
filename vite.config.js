19aec074549cefe885ea8740c2a9aa5f63b46398
Pawan Rai <pr90446@gmail.com>
2025-07-06 12:27:34 +0545

    both

--- vite.config.js
+++ vite.config.js
@@ -2,8 +2,10 @@ import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 import tailwindcss from "@tailwindcss/vite";
 
+const isGitHub = process.env.DEPLOY_TARGET === "GH";
+
 // https://vite.dev/config/
 export default defineConfig({
-  base: "/test-demo",
+  base: isGitHub ? "/your-repo-name/" : "/",
   plugins: [react(), tailwindcss()],
 });
